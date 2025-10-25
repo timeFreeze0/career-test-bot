a61c76fed9272dd8c0cb2ad7cc90e5612435f7b1
Ibragim Sarsenov <188056878+timeFreeze0@users.noreply.github.com>
2025-10-25 19:59:17 +0500

    chore(eslint): import .gitignore patterns into config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,9 +4,17 @@ import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
-  eslintConfigPrettier
+  eslintConfigPrettier,
+  includeIgnoreFile(gitignorePath, "Imported .gitignore patterns")
 );
